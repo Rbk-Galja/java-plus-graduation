3c30871e4cf2659f6236dafb2affec4f887aa0ae
User Galina <rbkgalja@yandex.ru>
2025-10-15 22:17:53 +0300

    Изменения по 1 этапу

--- stats/server/src/main/java/ru/practicum/StatServer.java
+++ stats/server/src/main/java/ru/practicum/StatServer.java
@@ -2,11 +2,7 @@ package ru.practicum;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@ConfigurationPropertiesScan
-@EnableDiscoveryClient
 @SpringBootApplication
 public class StatServer {
     public static void main(String[] args) {
